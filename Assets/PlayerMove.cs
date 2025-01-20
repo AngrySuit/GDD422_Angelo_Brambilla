d1f801cf50a9efac733a4dae7c8baf1bf76f1918
UOC\2401792 <angello.brambilla99@gmail.com>
2025-01-20 16:37:47 +0000

    Player Movement

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    [SerializeField] Rigidbody2D rb;
+
+    [SerializeField] Vector2 move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Move();
+    }
+
+
+
+    void Move()
+    {
+        // Gets the player input and changes the velocity of the character to it//
+        move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rb.velocity = move * 5;
+    }
+}
